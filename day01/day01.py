86c6fb7968aebaf16d1fe68cc67b732d54932696
maykcaldas <maykcaldas@gmail.com>
2021-12-17 16:53:51 -0300

    day01 changed

--- day01/day01.py
+++ day01/day01.py
@@ -1,22 +1,37 @@
-import os
+
 
 def read_input(name):
     with open(name,'r') as input:
         inp=input.readline()
     return inp
 
+def process_position(pos):
+    if pos == '(': return 1
+    elif pos == ')': return -1
+    else: 
+        print("Err") 
+        return 0
+
+def p1():
+    floor=0
+    inp=read_input('day01.dat')
+    for pos, k in enumerate(inp):
+        floor += process_position(k)
+    return floor
+
+def p2():
+    floor=0
+    basement=False
+    inp=read_input('day01.dat')
+    for pos, k in enumerate(inp):
+        floor += process_position(k)
+        if (floor==-1): return pos+1
+
+def main():
+    print(f"Part1: {p1()}")
+    print(f"Part2: {p2()}")
+
+if __name__ == "__main__":
+  main()
 
 
-floor=0
-first=True
-inp=read_input('2015/day1.dat')
-for pos, k in enumerate(inp):
-    if k == '(': floor+=1
-    elif k == ')': floor-=1
-    else: print("Err")
-    if floor == -1 and first:
-        first = False
-        firstMinus=pos+1
-
-print(floor)
-print(firstMinus)

fc25cec67f9e1e30b55fcb783a2a67ebd7155b98
maykcaldas <maykcaldas@gmail.com>
2021-12-17 22:27:51 -0300

    directory structure prepared

---
+++ day20/day20.py
@@ -0,0 +1,22 @@
+
+
+def read_input(name):
+    with open(name,'r') as input:
+        inp=input.readline()
+    return inp
+
+def p1():
+    pass
+
+
+def p2():
+    pass
+
+def main():
+    print(f"Part1: {p1()}")
+    print(f"Part2: {p2()}")
+
+if __name__ == "__main__":
+  main()
+
+
